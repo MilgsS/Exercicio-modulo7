16b43defd8d9402591628c5ec64de6cba7be6ac6
MilgsS <127366107+MilgsS@users.noreply.github.com>
2023-05-29 14:21:02 -0300

    Add files via upload

--- Js 2/main.js
+++ Js 2/main.js
@@ -1,12 +1,13 @@
 const formu = document.getElementById('form');
 var maior = document.getElementById('maior');
 var menor = document.getElementById('menor');
-const MDS = 'O numero a esquerda é mair que o da direita'
-const MDE = 'O da esquerda esta maior ou igual ao da direita. Tente Novamente';
+const MDS = document.getElementById('MDS')
+const MDE = document.getElementById('falha')
+const quase = document.getElementById('quase')
 
-form.addEventListener('submit', function(e){
+formu.addEventListener('submit', function(e) {
     e.preventDefault();
+    let correto = (maior.value)-(menor.value);
+    if (correto < 0) MDE.style.display = 'block', MDS.style.display='none', quase.style.display='none'
+    else MDS.style.display = 'block', MDE.style.display='none', quase.style.display='none'
 })
-correto = (maior.value) - (menor.value)
-if (correto<= 0 ) MDS.style.display = block;
-else falha.style.display = block
